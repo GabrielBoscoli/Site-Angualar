5efe25e297d9f289deee5ddeb1efa9eab2a2a985
Gabriel Oliveira Costa Boscoli <gboscoli@tecgraf.puc-rio.br>
2020-02-27 14:45:29 -0300

    Single Page Applications Part 1

---
+++ src/app/shared/promotions.ts
@@ -0,0 +1,14 @@
+import { Promotion } from './promotion';
+
+export const PROMOTIONS: Promotion[] = [
+    {
+      id: '0',
+      name: 'Promoção do Magui',
+      image: '/assets/images/buffet.png',
+      label: 'New',
+      price: '19.99',
+      featured: true,
+      // tslint:disable-next-line:max-line-length
+      description: 'Ao pedir essa promoção, você ganha um autógrafo do Magui.'
+    }
+  ];
