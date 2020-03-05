e2e2d4ee56d53eb00c204cd785d625521e308102
Gabriel Boscoli <gboscoli@tecgraf.puc-rio.br>
2020-03-05 12:53:59 -0300

    assignment 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSliderModule } from '@angular/material/slider';
 
 import { AppComponent } from './app.component';
 
@@ -53,6 +54,7 @@ import { MatDialogModule } from '@angular/material/dialog';
     MatToolbarModule,
     AppRoutingModule,
     FlexLayoutModule,
+    MatSliderModule,
     MatListModule,
     MatFormFieldModule,
     MatProgressSpinnerModule,
--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Dish } from '../shared/dish';
 import { DishService } from '../services/dish.service';
 
 import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { switchMap } from 'rxjs/operators';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dishdetail',
@@ -19,9 +20,54 @@ export class DishdetailComponent implements OnInit {
   prev: string;
   next: string;
 
+  commentaryForm: FormGroup;
+  @ViewChild('cform') commentaryFormDirective;
+  showPreview = false;
+
+  formErrors = {
+    'author': '',
+    'comment': ''
+  };
+
+  validationMessages = {
+    'author': {
+      'required':      'Author name is required.',
+      'minlength':     'Author name must be at least 2 characters long.'
+    },
+    'comment': {
+      'required':      'Comment is required.',
+    },
+  };
+
   constructor(private dishservice: DishService,
     private route: ActivatedRoute,
-    private location: Location) { }
+    private location: Location,
+    private fb: FormBuilder) { 
+      this.createForm();
+    }
+
+  createForm() {
+    this.commentaryForm = this.fb.group({
+      author: ['', [Validators.required, Validators.minLength(2)] ],
+      rating: 5,
+      comment: ['', Validators.required]
+    });
+    this.commentaryForm.valueChanges.subscribe(data => this.onValueChanged(data));
+
+    this.onValueChanged(); // (re)set validation messages now
+  }
+
+  onSubmit() {
+    this.commentaryForm.value.date = (new Date());
+    this.dish.comments.push(this.commentaryForm.value);
+    this.commentaryFormDirective.resetForm();
+    this.commentaryForm.reset({
+      author: '',
+      rating: 5,
+      comment: ''
+    });
+
+  }
 
   ngOnInit() {
     this.dishservice.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
@@ -37,6 +83,26 @@ export class DishdetailComponent implements OnInit {
 
   goBack(): void {
     this.location.back();
+  }
+
+  onValueChanged(data?: any) {
+    if (!this.commentaryForm) { return; }
+    const form = this.commentaryForm;
+    for (const field in this.formErrors) {
+      if (this.formErrors.hasOwnProperty(field)) {
+        // clear previous error message (if any)
+        this.formErrors[field] = '';
+        const control = form.get(field);
+        if (control && control.dirty && !control.valid) {
+          const messages = this.validationMessages[field];
+          for (const key in control.errors) {
+            if (control.errors.hasOwnProperty(key)) {
+              this.formErrors[field] += messages[key] + ' ';
+            }
+          }
+        }
+      }
+    }
   }
 
 }
